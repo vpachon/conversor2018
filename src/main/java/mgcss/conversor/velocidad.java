0849cd8af045337b32e7b3f1a010443ff0e1f6c3
pergendeveloper <pergendeveloper@gmail.com>
2018-03-22 17:50:22 +0100

    Implementación  + millas por segundo  + millas por hora  + pies segundos  + nudos naúticos

--- src/main/java/mgcss/conversor/velocidad.java
+++ src/main/java/mgcss/conversor/velocidad.java
@@ -8,4 +8,19 @@ package mgcss.conversor;
 
 public class velocidad {
    // TODO resto de métodos 
+   public static double millas_segundos( double valor ){
+       return valor * 5793.64;
+   }
+   
+   public static double millas_hora( double valor ){
+       return valor * 1.61;
+   }
+   
+   public static double pies_segundos( double valor ){
+       return valor * 1.1;
+   }
+   
+   public static double nudos_nauticos( double valor ){
+       return valor * 0.87;
+   }
 }
